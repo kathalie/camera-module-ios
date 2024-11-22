c349ce47e87b4dace80329183078be43b65eb997
Kathryn Verkhohliad <verkhogliadkate@gmail.com>
2024-11-22 13:18:11 +0100

    Implemented basic layout for gallery view

---
+++ CameraModule/UI/GalleryViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  GaleryCellView.swift
+//  CameraModule
+//
+//  Created by Kathryn Verkhogliad on 22.11.2024.
+//
+
+import UIKit
+
+class GalleryViewCell: UICollectionViewCell {
+    func config() {
+        
+    }
+}
